2a6476fb57edef89c606d5f20ea1b5deaa0e1078
Alicja Raszkowska <malavarena@gmail.com>
2018-04-01 18:38:52 -0700

    New post 2018-04-01 16:32:41

--- scripts/linear-gradients.js
+++ scripts/linear-gradients.js
@@ -22,7 +22,6 @@ function adjustGradient(angle, id) {
 }
 
 adjustLine(-150, 100, 600, 45, "black", "gradient-line");
-adjustLine(-150, 100, 600, 135, "lightgray", "perpendicular-line");
 adjustGradient(45, "js-gradient");
 
 var angle_form = document.getElementById("angle");
@@ -30,7 +29,36 @@ var ang = document.getElementById("ang");
 angle_form.oninput = function() {
   var angle = angle_form.value ? parseInt(angle_form.value) : 45;
   adjustLine(-150, 100, 600, angle, "black", "gradient-line");
-  adjustLine(-150, 100, 600, angle + 90, "lightgray", "perpendicular-line");
   adjustGradient(angle, "js-gradient");
   ang.textContent = angle;
+}
+
+function adjustGradientPercentages(angle, id) {
+  var gradient_box = document.getElementById(id);
+  gradient_box.style.background = 'linear-gradient('
+                                + angle + 'deg,'
+                                + 'white 0%,' + 'white 40%,'
+                                + 'orange 40%,' + 'orange 60%,'
+                                + 'white 60%,' + 'white 100%)';
+}
+
+function adjustGradientPixels(angle, id) {
+  var gradient_box = document.getElementById(id);
+  gradient_box.style.background = 'linear-gradient('
+                                + angle + 'deg,'
+                                + 'white 0%,' + 'white 80px,'
+                                + 'orange 80px,' + 'orange 120px,'
+                                + 'white 120px,' + 'white 100%)';
+}
+
+adjustGradientPercentages(0, "percentage-gradient");
+adjustGradientPixels(0, "pixel-gradient");
+
+var angle_form2 = document.getElementById("angle2");
+var ang2 = document.getElementById("ang2");
+angle_form2.oninput = function() {
+  angle2 = angle_form2.value ? parseInt(angle_form2.value) : 45;
+  adjustGradientPercentages(angle2, "percentage-gradient");
+  adjustGradientPixels(angle2, "pixel-gradient");
+  ang2.textContent = angle2;
 }
